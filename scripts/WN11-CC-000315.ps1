36be70a13d58c1aaf78eeaaf9e2cd2b81218bdc8
Anthony De Leon <236145713+anthonydeleon5@users.noreply.github.com>
2026-04-06 03:44:53 -0400

    Add script to disable elevated install privileges
    This script disables the 'Always install with elevated privileges' setting for both the local machine and current user by modifying the registry.

---
+++ scripts/WN11-CC-000315.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+    This PowerShell script disables 'Always install with elevated privileges' for both the local machine and current user.
+
+.NOTES
+    Author          : Anthony De Leon
+    LinkedIn        : linkedin.com/in/anthonydeleon5/
+    GitHub          : github.com/anthonydeleon5
+    Date Created    : 2026-04-06
+    Last Modified   : 2026-04-06
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000315
+
+.TESTED ON
+    Date(s) Tested  : 2026-04-06
+    Tested By       : Anthony De Leon
+    Systems Tested  : Windows 11
+    PowerShell Ver. : 5.1+
+
+.USAGE
+    Example syntax:
+    PS C:\> .\__remediation_template(WN11-CC-000315).ps1 
+#>
+
+$paths = @(
+    "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Installer",
+    "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Installer"
+)
+
+$valueName = "AlwaysInstallElevated"
+$valueData = 0
+
+foreach ($registryPath in $paths) {
+    if (-not (Test-Path $registryPath)) {
+        New-Item -Path $registryPath -Force | Out-Null
+    }
+
+    New-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -PropertyType DWord -Force | Out-Null
+    
+    Write-Host "Registry value '$valueName' set to '$valueData' at '$registryPath'."
+}
