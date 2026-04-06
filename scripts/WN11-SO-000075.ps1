3b5485ddc74cb605016bb94d3ba2ca19344c6a97
Anthony De Leon <236145713+anthonydeleon5@users.noreply.github.com>
2026-04-06 03:16:09 -0400

    Add PowerShell script for legal notice configuration

---
+++ scripts/WN11-SO-000075.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+    This PowerShell script configures the legal notice displayed before logon.
+
+.NOTES
+    Author          : Anthony De Leon
+    LinkedIn        : linkedin.com/in/anthonydeleon5/
+    GitHub          : github.com/anthonydeleon5
+    Date Created    : 2026-04-06
+    Last Modified   : 2026-04-06
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-SO-000075
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Run with administrative privileges.
+    PS C:\> .\WN11-SO-000075_remediation.ps1
+#>
+
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+
+Set-ItemProperty -Path $registryPath -Name "LegalNoticeCaption" -Value "Authorized Use Only"
+Set-ItemProperty -Path $registryPath -Name "LegalNoticeText" -Value "This system is for authorized use only."
+
+Write-Host "Legal logon banner configured."
