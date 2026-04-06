89246374c479d0e489a4d2b9e34f454adc1f5a6b
Anthony De Leon <236145713+anthonydeleon5@users.noreply.github.com>
2026-04-06 03:13:50 -0400

    Add PowerShell script for Handle Manipulation auditing
    This script enables auditing for Handle Manipulation success events.

---
+++ scripts/WN11-AU-000584.ps1
@@ -0,0 +1,29 @@
+<#
+.SYNOPSIS
+    This PowerShell script enables auditing for Handle Manipulation success events.
+
+.NOTES
+    Author          : Anthony De Leon
+    LinkedIn        : linkedin.com/in/anthonydeleon5/
+    GitHub          : github.com/anthonydeleon5
+    Date Created    : 2026-04-06
+    Last Modified   : 2026-04-06
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-AU-000584
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Run with administrative privileges.
+    PS C:\> .\WN11-AU-000584_remediation.ps1
+#>
+
+auditpol /set /subcategory:"Handle Manipulation" /success:enable
+
+Write-Host "Audit policy 'Handle Manipulation (Success)' enabled."
