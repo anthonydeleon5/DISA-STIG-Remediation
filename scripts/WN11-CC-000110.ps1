796b4bf9a7badb2c44a4e25f11672f27c46b27f1
Anthony De Leon <236145713+anthonydeleon5@users.noreply.github.com>
2026-04-06 03:24:36 -0400

    Create WN11-CC-000110.ps1

---
+++ scripts/WN11-CC-000110.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+    This PowerShell script disables printing over HTTP.
+
+.NOTES
+    Author          : Anthony De Leon
+    LinkedIn        : linkedin.com/in/anthonydeleon5/
+    GitHub          : github.com/anthonydeleon5
+    Date Created    : 2026-04-06
+    Last Modified   : 2026-04-06
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000110
+
+.TESTED ON
+    Date(s) Tested  : 2026-04-06
+    Tested By       : Anthony De Leon
+    Systems Tested  : Windows 11
+    PowerShell Ver. : 5.1+
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(WN11-CC-000110).ps1 
+#>
+
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers"
+$valueName = "DisableHTTPPrinting"
+$valueData = 1
+
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+
+New-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -PropertyType DWord -Force | Out-Null
+
+Write-Host "HTTP printing disabled."
